4377d4b51657e1351f906e67cf705cc2d4d6aeeb
jaygandhi18 <121659709+jaygandhi18@users.noreply.github.com>
2025-06-06 22:53:52 +0530

    updated max duration

--- app/(logged-in)/upload/page.tsx
+++ app/(logged-in)/upload/page.tsx
@@ -7,6 +7,8 @@ import { containerVariants } from "@/utils/constants";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
+export const maxDuration = 20;
+
 export default async function Page() {
     const user = await currentUser();
 
